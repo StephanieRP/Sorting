d12d1fe643089700c1779db678c87dc63b732501
Stephanie Patton <stephaniepatton@Stephanies-MacBook-Air.local>
2019-07-16 09:23:57 -0500

    formated function

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -17,7 +17,7 @@ def selection_sort(arr):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-for i in range(0, len(arr) - 1):
+    for i in range(0, len(arr) - 1):
         print(i)
         for j in range(0, len(arr) - 1 - i):
             print(j)
